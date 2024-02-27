165b530cc96da39ed9bcf42b1b443de5d23191a3
Rurek1101 <rurekus1101@gmail.com>
2024-02-27 14:01:11 +0000

    wyslanie pracy

--- cpp/cwiczenie/main.cpp
+++ cpp/cwiczenie/main.cpp
@@ -3,6 +3,30 @@
 using namespace std;
 
 int main(){
-	cout<<"witaj swiecie"<<endl;
+	float z;
+	float i;
+	float cp;
+	float sp;
+	float pb;
+	float paliwo;
+	float cena;
+	float cenaostateczna;
+	float iloscpostojow;
+	cout<<"podaj odległość podróży w kilometrach"<<endl;
+	cin>>z;
+	cout<<"podaj ilość pasażerów"<<endl;
+	cin>>i;
+	cout<<"podaj cenę paliwa za litr"<<endl;
+	cin>>cp;
+	cout<<"średnia spalania samochodu na 100km"<<endl;
+	cin>>sp;
+	cout<<"pojemność baku w litrach"<<endl;
+	cin>>pb;
+	paliwo = z * sp / 100 ;
+	cena = paliwo * cp ;
+	cenaostateczna = cena / i ;
+	iloscpostojow = paliwo / pb ;
+	cout<<"cena paliwa wyniesie:"<< cenaostateczna <<endl<<"postojów na stacji:"<< iloscpostojow <<endl;
 	return 0;
+
 }
