14b478f0ff3f08a89b4d8af39c9a6adb1370a133
Rurek1101 <rurekus1101@gmail.com>
2024-04-16 13:01:31 +0000

    wyslanie pracy

--- cpp/cwiczeniesort/main.cpp
+++ cpp/cwiczeniesort/main.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
 
-using namespace std
+using namespace std;
 
+bombel_sort(int x[],int y){
+for(int i=0;i<n;i++)
+    for(int j=1;j<y;j++)
+    if(x[j-1]>x[j])
+	swap(x[j-1],x[j]);
+}
 int main(){
-int t[4] = {5,3,6,4}
-	
-	return 0
+int x,y;
+cout<<"Witaj moge za ciebie posortowac liczby"endl;
+cout<<"Ile liczb zamierzasz posortowac";
+cin>>y;
+x = new int [n];
+for(int i=0;i<n;i++)
+    cin>>x[i];
+bombel_sort(x,n);
+for(int i=0;i<n;i++)
+    cout<<x[i]<<",";
+
+    return 0;
 }
