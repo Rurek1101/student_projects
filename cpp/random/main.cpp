a8c3f92c177d3b869c958397851ebc0db8d097fb
Rurek1101 <rurekus1101@gmail.com>
2024-04-23 13:02:12 +0000

    wyslanie pracy

--- cpp/random/main.cpp
+++ cpp/random/main.cpp
@@ -3,13 +3,8 @@
 using namespace std;
 
 int main(){
-	srand(time(NULL));
-	cout<<rand()<<endl;
-	cout<<rand()<<endl;
-	//losowanie w zakresie
-	int ile_liczb_w_przedziale = 2;
-	int liczba_startowa = 1;
-	int los = rand()%ile_liczb_w_przedziale + liczba_startowa;
-	cout<<los;
-	return 0;
+srand(time(NULL));
+int liczba
+
+return 0;
 }
