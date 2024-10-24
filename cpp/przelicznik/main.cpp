4891feda9465cdbdc77a2e310bd7ae3cb7751e77
Rurek1101 <rurekus1101@gmail.com>
2024-10-24 14:26:29 +0000

    commit from StudentMachine

--- cpp/przelicznik/main.cpp
+++ cpp/przelicznik/main.cpp
@@ -5,19 +5,19 @@ using namespace std;
 int main(){
 
 float liter;
-cout<<"Ile litrów wariacie?"<<endl;
+cout<<"Ile litrów wariacie? Przelicze je na galony"<<endl;
 cin>>liter;
 if (liter>0) {
     float gallon = (liter * 0.264172052);
     if (gallon<1) {
-	cout<<liter<<" litrów to dokładnie "<<gallon<<" galona";
+	cout<<liter<<" litrów to dokładnie "<<gallon<<" galona"<<endl;
 	}
     if (gallon>1) {
-	cout<<liter<<" litrów to dokładnie "<<gallon<<" galonów";
+	cout<<liter<<" litrów to dokładnie "<<gallon<<" galonów"<<endl;
 }
 }
 if (liter<0) {
-	cout<<"jednostka objętości nie może być ujemna :)";
+    cout<<"Jednostka objętości nie może być ujemna =)"<<endl;
 }
 return 0;
 }
